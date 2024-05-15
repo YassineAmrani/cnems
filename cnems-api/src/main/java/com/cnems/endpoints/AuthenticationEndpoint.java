05a63b65ac130b693ae73dfc325b198c91e32b44
Amrani Yassine <46766179+YassineAmrani@users.noreply.github.com>
2024-05-15 11:24:14 +0100

    Set up login and resgistration tested cases

--- cnems-api/src/main/java/com/cnems/endpoints/AuthenticationEndpoint.java
+++ cnems-api/src/main/java/com/cnems/endpoints/AuthenticationEndpoint.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.http.HttpResponse;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthenticationEndpoint {
 
     @Autowired
