aa3ae832e1bbd26207ebb931f7516f5814977a52
Amrani Yassine <46766179+YassineAmrani@users.noreply.github.com>
2024-05-22 14:50:18 +0100

    created users table with all its columns and refactored code to reflect that table

--- cnems-api/src/main/java/com/cnems/entities/User.java
+++ cnems-api/src/main/java/com/cnems/entities/User.java
@@ -2,6 +2,8 @@ package com.cnems.entities;
 
 import jakarta.persistence.*;
 
+import java.util.Date;
+
 @Entity
 @Table(name="users", schema = "public")
 public class User {
@@ -16,25 +18,41 @@ public class User {
     @Column(name = "password")
     private String password;
 
-    public User(long id, String username, String password) {
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "created_at")
+    private Date created_at;
+
+    public User(long id, String username, String password, String email, String role, Date created_at) {
         setId(id);
         setUsername(username);
         setPassword(password);
+        setEmail(email);
+        setRole(role);
+        setCreated_at(created_at);
     }
 
-    public User(String username, String password) {
+    public User(String username, String password, String email, String role, Date created_at) {
         setUsername(username);
         setPassword(password);
+        setEmail(email);
+        setRole(role);
+        setCreated_at(created_at);
     }
 
-    public User(){}
+    public User() {
+    }
 
     public long getId() {
         return id;
     }
 
-    public void setId(long identifier) {
-        id = identifier;
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -51,5 +69,29 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
     }
 }
